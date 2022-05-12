6de6cb03b379ecb0185e84952f576c9202eb1d75
essa eayd abu kheir <esaaeyad22@gmail.com>
2022-05-12 05:27:59 -0700

    class-16

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './ Component/Home-component';
-import Footer from './ Component/Footer-component';
-import Header from './ Component/Header-component';
-import Touerco from './ Component/Tours-component'; 
+import Home from './ Component/Home/Home-component';
+import Footer from './ Component/Footer/Footer-component';
+import Header from './ Component/Header/Header-component';
+import Touerco from './ Component/Touerco/Tours-component'; 
   
-
 const tours=require('./data/data.json')
+
 function App() {
   return (
     <>
-    <Header/>
-   
-    
-    {
-      tours.map(name=>{
-        return(
-          <Touerco
-           img={name.image} 
-            title={name.name}/>
-
-        )
-      })
-    }
-   <Footer/>
+   <Home tours={tours} />
   </>
 
 
