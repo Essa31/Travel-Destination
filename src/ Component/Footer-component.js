33b9f3f5e04f1e518b067ff21e9bc7d23cd947f9
essa eayd abu kheir <esaaeyad22@gmail.com>
2022-05-12 04:31:16 -0700

    class-16

---
+++ src/ Component/Footer-component.js
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+      <>
+    <p> &copy; copyrigth 2022 Essa Eayd Abu Kheir</p>
+       <a href="https://web.facebook.com/home.php">facebook</a>
+       <p></p>
+       <a href="https://www.youtube.com/ ">youtube</a>
+        
+      </>
+    );
+  }
+  
+  export default Footer;
